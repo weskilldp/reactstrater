214639acc72b5c5cf314420062e71e08cc6bc138
Amit Deshmukh <adeshmukh@delaplex.in>
2019-05-09 19:07:49 +0530

    Testing 1

--- src/components/App.js
+++ src/components/App.js
@@ -57,7 +57,7 @@ class App extends React.PureComponent {
   render() {
     // NOTE: If you need to add or modify header, footer etc. of the app,
     // please do that inside the Layout component.
-    return React.Children.only(this.props.children);
+    return React.Children.not.only(this.props.children);
   }
 }
 
